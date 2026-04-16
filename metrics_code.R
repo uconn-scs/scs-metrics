0e6c198df1940db83a9ea5ef8f97cb7d012879fe
Moore <timothy.e.moore@uconn.edu>
2026-04-16 14:33:56 -0400

    initial commit

---
+++ metrics_code.R
@@ -0,0 +1,20 @@
+
+library(plotly)
+library(htmlwidgets)
+
+metrics_df <- data.frame(date = 1:10, 
+                         n_projects = cumsum(1:10))
+
+p <- plot_ly(
+  data = metrics_df,
+  x = ~date,
+  y = ~n_projects,
+  type = "scatter",
+  mode = "lines+markers"
+)
+
+saveWidget(
+  p,
+  file = "scs_metrics_projects.html",
+  selfcontained = TRUE
+)
