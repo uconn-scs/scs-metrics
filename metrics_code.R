96e8bb9ec3c8f815becd88c15fa13c53dcbe9d48
Moore <timothy.e.moore@uconn.edu>
2026-04-16 15:22:58 -0400

    real data

--- metrics_code.R
+++ metrics_code.R
@@ -1,17 +1,56 @@
 
+library(ggplot2)
 library(plotly)
 library(htmlwidgets)
 
-metrics_df <- data.frame(date = 1:10, 
-                         n_projects = cumsum(1:10))
+metrics_df <- data.frame(year = c("FY26"),
+                         date = factor(c("Jul", 
+                                  "Aug",
+                                  "Sep",
+                                  "Oct",
+                                  "Nov",
+                                  "Dec",
+                                  "Jan",
+                                  "Feb",
+                                  "Mar",
+                                  "Apr"),
+                                  levels = c("Jul", 
+                                    "Aug",
+                                    "Sep",
+                                    "Oct",
+                                    "Nov",
+                                    "Dec",
+                                    "Jan",
+                                    "Feb",
+                                    "Mar",
+                                    "Apr"),
+                                   ordered = TRUE),
+                                  
+                         n_projects = cumsum(c(1,
+                                             7,
+                                             4,
+                                             6,
+                                             0,
+                                             4,
+                                             7,
+                                             12,
+                                             14,
+                                             16)))
+n_proj_p <- 
+ggplot(data= metrics_df, aes(x = date, y = n_projects, 
+                             color = year))+
+  geom_point(size = 4)+
+  scale_color_manual(values ="#C00000")+
+  geom_line(aes(group = 1))+
+  theme_bw(base_size = 18)+
+  labs(x = "Month", 
+       y = "Number of Projects (Cumulative)", 
+       title = "Number of Full Consulting Projects - FY26", 
+        subtitle = "For FY 2026")+
+  theme(legend.position = "none")+
+  coord_fixed(ratio = 1/10)
 
-p <- plot_ly(
-  data = metrics_df,
-  x = ~date,
-  y = ~n_projects,
-  type = "scatter",
-  mode = "lines+markers"
-)
+p <- ggplotly(n_proj_p)
 
 saveWidget(
   p,
